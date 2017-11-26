33ad967601c235ec6bb269b22ea096242af00df1
Benjamin <malkassian.b@hotmail.com>
2017-11-26 17:10:34 +0100

    Camera11

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
-import { Camera, CameraOptions } from '@ionic-native/camera';
+import {Component} from '@angular/core';
+import {NavController, AlertController} from 'ionic-angular';
+import {Camera, CameraOptions} from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
   public photos : any;
   public base64Image : string;
+  constructor(public navCtrl : NavController, private camera : Camera, private alertCtrl : AlertController) {}
 
-  constructor(public navCtrl: NavController, private camera : Camera, private alertCtrl : AlertController) {
-
+  ngOnInit() {
+    this.photos = [];
   }
 
   deletePhoto(index) {
-    let confirm = this.alertCtrl.create({
+    let confirm = this
+      .alertCtrl
+      .create({
         title: 'Sure you want to delete this photo? There is NO undo!',
         message: '',
         buttons: [
@@ -28,7 +32,10 @@ export class HomePage {
             text: 'Yes',
             handler: () => {
               console.log('Agree clicked');
-              this.photos.splice(index, 1);
+              this
+                .photos
+                .splice(index, 1);
+              //return true;
             }
           }
         ]
@@ -36,20 +43,27 @@ export class HomePage {
     confirm.present();
   }
 
-  takePicture(){
+  takePhoto() {
     const options : CameraOptions = {
-      quality: 50, // picture quality
-      destinationType: this.camera.DestinationType.DATA_URL,
+      quality: 50,
+      destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     }
-    this.camera.getPicture(options) .then((imageData) => {
-       let base64Image = 'data:image/jpeg;base64,' + imageData;
-      this.photos.push(this.base64Image);
-      this.photos.reverse();
-    }, (err) => {
-      console.log(err);
-    });
+    this
+      .camera
+      .getPicture(options)
+      .then((imageData) => {
+        this.base64Image = "file://" + imageData;
+        this
+          .photos
+          .push(this.base64Image);
+        this
+          .photos
+          .reverse();
+      }, (err) => {
+        console.log(err);
+      });
   }
 
 }
