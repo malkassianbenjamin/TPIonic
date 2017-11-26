fe40640153a83afb411659fc6544360c74c7d9fc
Benjamin <malkassian.b@hotmail.com>
2017-11-26 17:47:29 +0100

    Camera16

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -66,6 +66,30 @@ export class HomePage {
       }, (err) => {
         console.log(err);
       });
+      let confirm = this
+        .alertCtrl
+        .create({
+          title: 'Sure you want to delete this photo? There is NO undo!',
+          message: '',
+          buttons: [
+            {
+              text: 'No',
+              handler: () => {
+                console.log('Disagree clicked');
+              }
+            }, {
+              text: 'Yes',
+              handler: () => {
+                console.log('Agree clicked');
+                this
+                  .photos
+                  .splice(index, 1);
+                //return true;
+              }
+            }
+          ]
+        });
+      confirm.present();
   }
 
   alertPhoto(){
