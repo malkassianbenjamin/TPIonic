1861339dbc05e0025cc11ee82cca553afd056507
Benjamin <malkassian.b@hotmail.com>
2017-11-26 17:24:24 +0100

    Camera13

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -46,7 +46,7 @@ export class HomePage {
   takePhoto() {
     const options : CameraOptions = {
       quality: 50,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     }
