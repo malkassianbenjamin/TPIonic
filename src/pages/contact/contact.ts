a085153bdeece16fea12e0be2344129cef2d90fc
Benjamin <malkassian.b@hotmail.com>
2018-01-09 09:06:56 +0100

    'commitRendu'

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -20,9 +20,9 @@ export class ContactPage {
   @ViewChild('map') mapElement: ElementRef;
   map: GoogleMap;
 
-  constructor(public navCtrl: NavController,
-              public geolocation: Geolocation,
-              public googleMaps: GoogleMaps) {}
+  constructor(private navCtrl: NavController,
+              private geolocation: Geolocation,
+              private googleMaps: GoogleMaps) {}
 
   ngAfterViewInit(){
     this.initMap();
@@ -30,6 +30,17 @@ export class ContactPage {
 
   initMap(){
     let element = this.mapElement.nativeElement;
-    this.map = this.googleMaps.create(element)
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+    this.map = GoogleMaps.create(element, mapOptions);
+    console.log(this.map);
   }
 }
